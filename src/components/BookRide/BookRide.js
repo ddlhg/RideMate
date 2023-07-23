03a3a67badb78a36344ff49db468c82065d74d30
Ameer Khan <angello.khan21@gmail.com>
2023-07-23 07:52:24 -0400

    changes
    added book ride
    and sign up with edu email

--- src/components/BookRide/BookRide.js
+++ src/components/BookRide/BookRide.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
 import "./BookRide.css";
+import { firestore } from "./firebase";
 
-function BookRide(){
-    return(<div></div>)
-}
+//function BookRide(){
+    //return(<div></div>)
+//}
+
+//export default BookRide;
+// RequestRide.js
+
+
+function BookRide  () {
+  const [location, setLocation] = useState("");
+
+  const handleRequest = () => {
+    if (location.trim() !== "") {
+      // Save the ride request to Firestore
+      firestore.collection("rideRequests").add({
+        location: location.trim(),
+        timestamp: new Date(),
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Book a Ride</h2>
+      <input
+        type="text"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        placeholder="Enter your location"
+      />
+      <button onClick={handleRequest}>Book Ride</button>
+    </div>
+  );
+};
 
 export default BookRide;
