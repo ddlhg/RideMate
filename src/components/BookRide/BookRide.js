9da4d3e18c2eb6712576786089d3c5b317237e1e
Gikai Andrews <gnaboys@gmail.com>
2023-08-04 20:26:53 -0400

    fixed register form

--- src/components/BookRide/BookRide.js
+++ src/components/BookRide/BookRide.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import "./BookRide.css";
-import App.js;
+// import App.js;
 // import { firestore } from "./firebase";
 
 //function BookRide(){
@@ -12,7 +12,7 @@ import App.js;
 
 
 function BookRide  () {
-  const [location, setLocation] = useState("");
+  // const [location, setLocation] = useState("");
 
   // const handleRequest = () => {
   //   if (location.trim() !== "") {
@@ -24,25 +24,25 @@ function BookRide  () {
   //     setLocation("");
   //   }
   // };
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWxsbzIxIiwiYSI6ImNsa2NsdWYzMzBoYnozZHBqOGc1YXlkOWcifQ.TZ9pEPeO8mgXFGEnkkh8gA';
+  // mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWxsbzIxIiwiYSI6ImNsa2NsdWYzMzBoYnozZHBqOGc1YXlkOWcifQ.TZ9pEPeO8mgXFGEnkkh8gA';
 
-  const App = () => {
-    useEffect(() => {
+  // const App = () => {
+  //   useEffect(() => {
       // Create a map instance
-      const map = new mapboxgl.Map({
-        container: 'map-container', // HTML element ID where the map will be displayed
-        style: 'mapbox://styles/mapbox/streets-v11', // Map style URL
-        center: ["-80.1010 W, 26.3705 N"], // Center coordinates [longitude, latitude]
-        zoom: 12, // Initial zoom level
-      });
+      // const map = new mapboxgl.Map({
+      //   container: 'map-container', // HTML element ID where the map will be displayed
+      //   style: 'mapbox://styles/mapbox/streets-v11', // Map style URL
+      //   center: ["-80.1010 W, 26.3705 N"], // Center coordinates [longitude, latitude]
+      //   zoom: 12, // Initial zoom level
+      // });
   
       // Add map controls, markers, and other map features as needed
   
-      return () => {
+      // return () => {
         // Clean up the map instance on unmount
-        map.remove();
-      };
-    }, []);
+    //     map.remove();
+    //   };
+    // }, []);
   
   
   return (
@@ -50,8 +50,7 @@ function BookRide  () {
       <h2>Book a Ride</h2>
       <input
         type="text"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
+        // onChange={(e) => setLocation(e.target.value)}
         placeholder="Enter your location"
       />
       <button>Book Ride</button>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-import {UserProvider} from './components/UserContext/UserContext'
-import "@cometchat-pro/chat/dist/css/cometchat.min.css";
-=======
->>>>>>> 2a4cf7ec9e68a63dcd4f1e1ac6889a4c02d52b46
+// import "@cometchat-pro/chat/dist/css/cometchat.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
